9ecede7c78042ca94dc42befa8c4499ad043eced
Dawson-sk <dawson.skelley@gmail.com>
2025-10-24 01:26:14 -0400

    Fixed unit test

--- internal/auth/get_api_ket_test.go
+++ internal/auth/get_api_ket_test.go
@@ -19,7 +19,7 @@ func TestGetAPIKey(t *testing.T) {
 		},
 		{
 			key:       "Authorization",
-			expectErr: "no authorizatio header",
+			expectErr: "no authorization header",
 		},
 		{
 			key:       "Authorization",
